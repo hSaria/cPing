cbaf43f991e7ce778ce7614e4e5676aae1611073
Saria Hajjar <sariahajjar@gmail.com>
2019-07-14 20:43:28 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,13 @@
+Pinger checks if hosts are responding using ICMP echo or TCP SYN.
+
+> Plenty of other solutions out there; this one is just a quick and short solution.
+
+![alt text](https://github.com/hSaria/ClusterTerminal/raw/master/.github/example_1.png "Example output")
+
+# Installation
+
+    pip3 install pinger
+
+# Usage
+
+    pinger host1 host2
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+REQUIRES_PYTHON = '>=3.6.0'
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pinger',
+    author='hSaria',
+    author_email='ping@heysaria.com',
+    classifiers=[
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3', 'Topic :: Internet',
+        'Topic :: System :: Networking :: Monitoring'
+    ],
+    description='Concurrent multi-host ping (ICMP and TCP)',
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires=REQUIRES_PYTHON,
+    scripts=['pinger'],
+    url='https://github.com/hSaria/Pinger',
+    version='0.0.1',
+)
