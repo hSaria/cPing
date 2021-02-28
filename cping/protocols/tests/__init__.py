3207074b418d5270216367683c5f4ba328d2acdb
Saria Hajjar <sariahajjar@gmail.com>
2021-02-28 14:55:23 +0000

    Speed up change_interval test

--- cping/protocols/tests/__init__.py
+++ cping/protocols/tests/__init__.py
@@ -4,17 +4,26 @@ import time
 
 def ping_change_interval(test_case, protocol):
     """Change the interval in the middle of the test."""
-    protocol.interval = 1
+    protocol.interval = 0.5
 
     host = protocol('1.2.3.4')
     host.start()
 
-    time.sleep(0.5)
-    protocol.interval = 0.5
+    # Change the interval in the middle of the first probe (takes effect after)
+    time.sleep(protocol.interval / 2)
+    protocol.interval = protocol.interval / 2
 
-    # Remainder of first probe, two full probes, and stop during the fourth
-    # ----->|<----->|<----->|<---
-    time.sleep(0.5 + 0.5 + 0.5 + 0.25)
+    # Remainder of first probe, one full probe, and stop during the third
+    # ----->|<----->|<---
+    time.sleep(protocol.interval * 2.5)
     host.stop(block=True)
 
-    test_case.assertEqual(len(host.results), 4)
+    test_case.assertEqual(len(host.results), 3)
+
+
+def ping_loop_once(host):
+    """Start the host and run a single iteration."""
+    host.protocol.wait = lambda *_: host.stop_signal.set()
+    host.start()
+    host.stop_signal.wait()
+    host.stop_signal.clear()
